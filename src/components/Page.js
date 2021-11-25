62a7d73521da38e94f8ba5912caca0946e87349d
Brooke Chalmers <breq@breq.dev>
2021-11-24 22:35:06 -0500

    Switch the background color on scroll to seamlessly switch the overscroll color based on if the top or bottom is overscrolled

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,12 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import SEOHelmet from "./SEOHelmet"
 
 export default function Page({ children, className }) {
+    useEffect(() => {
+        const handler = (event) => {
+            if (window.scrollY <= 100) {
+                document.body.classList.add("bg-panpink")
+                document.body.classList.remove("bg-panblue")
+            } else if (
+                window.scrollY >=
+                document.body.scrollHeight - window.innerHeight - 100
+            ) {
+                document.body.classList.remove("bg-panpink")
+                document.body.classList.add("bg-panblue")
+            }
+        }
+
+        handler()
+        window.addEventListener("scroll", handler)
+
+        return () => {
+            window.removeEventListener("scroll", handler)
+        }
+    }, [])
+
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-white">
             <SEOHelmet
                 title="hey, i'm brooke."
                 description="welcome to my little patch of internet. here you'll find my projects over the years."
