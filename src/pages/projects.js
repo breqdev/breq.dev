bbef31332dc121b9d3e13a121bdb1c1e2328b418
Brooke Chalmers <breq@breq.dev>
2021-12-21 03:26:15 -0500

    Add to project card

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -36,6 +36,7 @@ export const query = graphql`
                     frontmatter {
                         title
                         subtitle
+                        tags
                         image {
                             childImageSharp {
                                 gatsbyImageData(
