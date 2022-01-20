585a72d44c3f9333bbace2396a888c43e9c44453
Brooke <breq@breq.dev>
2022-01-19 20:12:33 -0500

    unused import

--- src/components/index/IndexCanvas.js
+++ src/components/index/IndexCanvas.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import useScroll from "../models/useScroll";
 
