c6001f9f6ecf54f7658319057bfee16df014cc50
Brooke <breq@breq.dev>
2022-02-01 21:59:35 -0500

    Use sine wave interpolation

--- src/components/index/Greeting.js
+++ src/components/index/Greeting.js
@@ -6,11 +6,27 @@ function InnerLogo(props) {
   const model = useRef();
 
   useScroll((scroll, height) => {
+    const movement_start = height * (0 / 6);
+    const movement_end = height * (4 / 6);
+
+    const final_position = -height / 400;
+
     if (model.current) {
-      if (scroll < height / 2) {
-        model.current.position.set(-scroll / 200, 0, 0);
+      if (scroll < movement_start) {
+        model.current.position.set(0, 0, 0);
+      } else if (scroll < movement_end) {
+        const movement_progress =
+          (scroll - movement_start) / (movement_end - movement_start);
+
+        const wave_position = movement_progress * Math.PI + Math.PI / 2;
+
+        model.current.position.set(
+          (final_position / 2) * -(Math.sin(wave_position) - 1),
+          0,
+          0
+        );
       } else {
-        model.current.position.set(-height / 400, 0, 0);
+        model.current.position.set(final_position, 0, 0);
       }
 
       model.current.rotation.y = 2e-3 * scroll;
