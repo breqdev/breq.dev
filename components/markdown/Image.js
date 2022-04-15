cd9687566afaf4dee7f13ddc491c8153dd33ef44
Brooke Chalmers <breq@breq.dev>
2022-04-15 13:08:37 -0400

    images too big

--- components/markdown/Image.js
+++ components/markdown/Image.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function MarkdownImage(props) {
   return (
-    <div className="flex w-full justify-center">
-      <Image {...props} src={"/images/" + props.src} className="text-center" />
+    <div className="my-4 mx-auto w-full max-w-5xl">
+      <Image {...props} src={"/images/" + props.src} />
     </div>
   );
 }
