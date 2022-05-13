29963c4694faef35dfeb5c1492d06adb081b143b
Brooke Chalmers <breq@breq.dev>
2022-05-13 01:52:50 -0400

    Remove the search bar (for now)

--- components/Navbar.js
+++ components/Navbar.js
@@ -73,7 +73,7 @@ export default function Navbar() {
 
           <div className="flex-grow" />
 
-          <Search onSelect={handleSelect} />
+          {/* <Search onSelect={handleSelect} /> */}
         </div>
       </div>
     </nav>
