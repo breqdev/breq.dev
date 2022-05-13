73769ba7a0c48597b6dc7ca0c8ea9e26d0410e72
Brooke Chalmers <breq@breq.dev>
2022-05-13 01:49:01 -0400

    center images properly

--- components/markdown/Image.js
+++ components/markdown/Image.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function MarkdownImage(props) {
   return (
-    <div className="my-4 mx-auto w-full max-w-5xl">
+    <div className="my-4 mx-auto flex w-full max-w-5xl justify-center">
       <Image alt="" {...props} src={"/images/" + props.src} />
     </div>
   );
