79f291abf20cf206c8d7f4fc2ee36a72022c33a0
Brooke Chalmers <breq@breq.dev>
2022-05-13 03:04:49 -0400

    fix flash of large font awesome icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 import "katex/dist/katex.min.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
