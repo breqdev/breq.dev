ea2f7a3a7b366cfc01bf133edd645e9ae506fd2c
Brooke Chalmers <breq@breq.dev>
2022-05-21 09:42:10 -0400

    add favicon etc

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,27 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#9f00a7" />
+          <meta name="theme-color" content="#ffffff" />
+
+          <link
             href="https://fonts.googleapis.com/css2?family=Nunito&display=swap"
             rel="stylesheet"
           />
