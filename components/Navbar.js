5164da5653ed343ecdce364b548dd5fb5665807c
Brooke Chalmers <breq@breq.dev>
2022-06-21 15:53:05 -0400

    remove unused import from navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHamburger } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import Search from "./Search";
 
 function SkipNavigation() {
   return (
