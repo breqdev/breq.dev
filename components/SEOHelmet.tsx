5e7d320d8e0de7a5d9a18ceabdd4e490ac1a8f5f
Brooke Chalmers <breq@breq.dev>
2022-06-21 23:10:26 -0400

    switch to a 2:1 embed image

--- components/SEOHelmet.tsx
+++ components/SEOHelmet.tsx
@@ -15,16 +15,13 @@ export default function SEOHelmet({ title, description, image }: SEOProps) {
 
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
-      <meta name="og:image" content={image || "/opengraph/pansexual.jpg"} />
+      <meta name="og:image" content={image || "/opengraph.jpg"} />
       <meta name="og:url" content="https://breq.dev/" />
       <meta name="og:site_name" content="breq.dev" />
       <meta name="og:type" content="website" />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta
-        name="twitter:image"
-        content={image || "https://breq.dev/opengraph/pansexual.jpg"}
-      />
+      <meta name="twitter:image" content={image || "/opengraph.jpg"} />
     </Head>
   );
 }
