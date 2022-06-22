7b1185d2a6fe139a0cd3f7309248aa7af9d67408
Brooke Chalmers <breq@breq.dev>
2022-06-21 23:23:56 -0400

    twitter absolute path to image

--- components/SEOHelmet.tsx
+++ components/SEOHelmet.tsx
@@ -8,6 +8,8 @@ type SEOProps = {
 };
 
 export default function SEOHelmet({ title, description, image }: SEOProps) {
+  const imageOrDefault = image || "opengraph.jpg";
+
   return (
     <Head>
       <title>{title}</title>
@@ -15,7 +17,7 @@ export default function SEOHelmet({ title, description, image }: SEOProps) {
 
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
-      <meta name="og:image" content={image || "/opengraph.jpg"} />
+      <meta name="og:image" content={imageOrDefault} />
       <meta name="og:url" content="https://breq.dev/" />
       <meta name="og:site_name" content="breq.dev" />
       <meta name="og:type" content="website" />
@@ -23,6 +25,10 @@ export default function SEOHelmet({ title, description, image }: SEOProps) {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@breqdev" />
       <meta name="twitter:creator" content="@breqdev" />
+      <meta
+        name="twitter:image"
+        content={"https://breq.dev/" + imageOrDefault}
+      />
     </Head>
   );
 }
