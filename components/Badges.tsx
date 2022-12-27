a3b6dc5537d74ad5a42cef893c15588ffbcf34eb
Brooke Chalmers <breq@breq.dev>
2022-12-27 15:25:25 -0500

    add placeholder for philo :)

--- components/Badges.tsx
+++ components/Badges.tsx
@@ -46,13 +46,18 @@ const BADGES = [
     image: "/badges/dillydally.png",
     url: "https://dillydally414.github.io/",
   },
+  {
+    name: "philomena",
+    placeholder: "philo",
+    url: "https://philo.gay/",
+  },
 ];
 
 export default function Badges() {
   return (
     <div className="md:pr-60">
       <div className="flex flex-wrap items-start justify-center gap-2 sm:justify-start">
-        {BADGES.map(({ name, image, url }) => (
+        {BADGES.map(({ name, image, url, placeholder }) => (
           <a
             href={url}
             key={name}
@@ -60,7 +65,12 @@ export default function Badges() {
             className="flex-shrink-0 outline-4 outline-panpink focus-visible:outline"
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img width={88} height={31} src={image} alt={name} />
+            {image && <img width={88} height={31} src={image} alt={name} />}
+            {placeholder && (
+              <div className="flex h-full items-center justify-center border-2 border-dashed border-white">
+                <span className="text-white">{placeholder}</span>
+              </div>
+            )}
           </a>
         ))}
       </div>
