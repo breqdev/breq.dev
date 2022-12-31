5a4647e6f99087fc64ef0b9f80b898e03bcfbbbd
Brooke Chalmers <breq@breq.dev>
2022-12-31 17:53:19 -0500

    fix pronouns handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -111,9 +111,11 @@ function Projects({ data }: { data: (BasicMarkdownInfo & ProjectInfo)[] }) {
 }
 
 function usePronouns() {
-  const [pronouns] = React.useState(() =>
-    Math.random() > 0.95 ? "she/they" : "she/her"
-  );
+  const [pronouns, setPronouns] = React.useState("she/her");
+
+  useEffect(() => {
+    setPronouns(Math.random() > 0.95 ? "she/they" : "she/her");
+  }, []);
 
   return pronouns;
 }
