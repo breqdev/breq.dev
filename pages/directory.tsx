a2db959f705de2e2fcac40d2d400f28eaa094cf2
Brooke Chalmers <breq@breq.dev>
2023-06-23 23:58:28 -0400

    mobile friendly directory

--- pages/directory.tsx
+++ pages/directory.tsx
@@ -3,8 +3,6 @@ import { BADGES } from "../utils/badges";
 import Page from "../components/Page";
 import SEOHelmet from "../components/SEOHelmet";
 
-const SCALE = 2.0;
-
 export default function Directory() {
   return (
     <Page>
@@ -15,28 +13,23 @@ export default function Directory() {
       <h1 className="mx-auto my-16 max-w-2xl text-center font-display text-6xl">
         friends of breq
       </h1>
-      <div className="mx-auto my-16 flex w-full max-w-2xl flex-col gap-4">
+      <div className="mx-auto my-16 flex w-full max-w-2xl flex-col gap-4 px-4">
         {BADGES.filter((b) => b.tag !== "breq").map(
           ({ name, image, url, placeholder, tag, bio }) => (
             <a
               href={url}
-              className="flex flex-row items-center gap-4"
+              className="flex flex-col items-center gap-4 md:flex-row"
               key={name}
             >
               <div
                 style={{
                   imageRendering: "pixelated",
-                  width: 88 * SCALE,
-                  height: 31 * SCALE,
+                  aspectRatio: "88 / 31",
                 }}
+                className="w-full max-w-md flex-shrink-0 md:w-[176px]"
               >
                 {image /* eslint-disable-next-line @next/next/no-img-element */ && (
-                  <img
-                    width={88 * SCALE}
-                    height={31 * SCALE}
-                    src={image}
-                    alt={name}
-                  />
+                  <img className="w-full" src={image} alt={name} />
                 )}
                 {placeholder && (
                   <div className="flex h-full items-center justify-center border-2 border-dashed border-white">
@@ -44,7 +37,7 @@ export default function Directory() {
                   </div>
                 )}
               </div>
-              <div className="flex flex-grow flex-col">
+              <div className="flex flex-grow flex-col items-center md:items-start">
                 <div className="flex items-center gap-1">
                   {tag && (
                     <>
