5d0059fa39198c324c14dcb1d5607065420d9c2c
Brooke Chalmers <breq@breq.dev>
2024-01-26 10:05:02 -0500

    Add Sentry integration

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e878b838585025c4cbfd28afaa3863cc@o4506638160691200.ingest.sentry.io/4506638168424448",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
