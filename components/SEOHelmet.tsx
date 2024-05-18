e0d5d7c694db242ecf9ddcce473953c02697b0dd
Brooke Chalmers <breq@breq.dev>
2024-05-17 21:44:39 -0400

    remove default opengraph image (it didn't rly add anything)

--- components/SEOHelmet.tsx
+++ components/SEOHelmet.tsx
@@ -8,8 +8,6 @@ type SEOProps = {
 };
 
 export default function SEOHelmet({ title, description, image }: SEOProps) {
-  const imageOrDefault = image || "/opengraph.jpg";
-
   return (
     <>
       <Head>
@@ -19,7 +17,7 @@ export default function SEOHelmet({ title, description, image }: SEOProps) {
 
         <meta name="og:title" content={title} />
         <meta name="og:description" content={description} />
-        <meta name="og:image" content={imageOrDefault} />
+        {image && <meta name="og:image" content={image} />}
         <meta name="og:url" content="https://breq.dev/" />
         <meta name="og:site_name" content="breq.dev" />
         <meta name="og:type" content="website" />
@@ -27,10 +25,9 @@ export default function SEOHelmet({ title, description, image }: SEOProps) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@breqdev" />
         <meta name="twitter:creator" content="@breqdev" />
-        <meta
-          name="twitter:image"
-          content={"https://breq.dev" + imageOrDefault}
-        />
+        {image && (
+          <meta name="twitter:image" content={"https://breq.dev" + image} />
+        )}
       </Head>
       <a rel="me" href="https://tacobelllabs.net/@breq" className="hidden"></a>
     </>
