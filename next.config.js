aa2502b762de151e461df1bc253d45f9d26ded22
Brooke Chalmers <breq@breq.dev>
2024-10-16 19:57:21 -0400

    set up bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.txt/,
@@ -13,7 +17,7 @@ module.exports = {
     return config;
   },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-};
+});
 
 // Injected content via Sentry wizard below
 
