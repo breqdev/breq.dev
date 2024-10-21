a272600977fb4cfbf68eee0a94776eaf2c24231a
Brooke Chalmers <breq@breq.dev>
2024-10-20 22:46:34 -0400

    add /about page

---
+++ pages/about.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Page from "../components/Page";
+import SEOHelmet from "../components/SEOHelmet";
+import AboutText from "../components/index/AboutText";
+import peace from "../public/images/peace.png";
+
+export default function About() {
+  return (
+    <Page className="bg-black px-4 py-4 text-white md:py-12">
+      <SEOHelmet
+        title="breq.dev | about me"
+        description="information about brooke madeline chalmers."
+      />
+
+      <div className="mx-auto grid w-full max-w-5xl grid-cols-1 gap-8 md:grid-cols-[1fr,2fr]">
+        <div className="relative mx-auto max-w-md">
+          <Image src={peace} alt="" className="md:rounded-full" />
+          <h1 className="absolute bottom-2 left-0 right-0 text-center font-display text-3xl md:hidden">
+            <span className="rounded-xl bg-black px-1.5 font-bold text-panpink">
+              brooke chalmers
+            </span>
+          </h1>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="hidden text-center font-display text-5xl md:block md:text-left">
+            brooke madeline chalmers
+          </h1>
+          <section className="mx-auto w-full font-display text-lg">
+            <AboutText />
+          </section>
+        </div>
+      </div>
+    </Page>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ import { BasicMarkdownInfo } from "../utils/markdown";
 import Link from "next/link";
 import generateRssFeed from "../utils/generateRSS";
 import HCard from "../components/HCard";
+import AboutText from "../components/index/AboutText";
 
 const Background = React.lazy(() => import("../components/index/IndexCanvas"));
 
@@ -223,36 +224,8 @@ export default function Index(props: {
               about me
             </h2>
           )}
-          <p>
-            <span className="font-bold text-panpink">hey, i'm brooke</span>, and
-            i'm here to learn, create, and have fun.
-            <br />
-            <br />
-            i'm passionate about embedded systems, backend engineering, web dev,
-            audio, and bodging things together in creative ways.
-            <br />
-            <br />
-            my favorite tools are Python, React, Rust, and Linux. most of my
-            work nowadays is completely digital, but i'm still comfortable with
-            a soldering iron and a breadboard.
-            <br />
-            <br />
-            i believe that the only way to learn something fully is to be
-            creative with it. you can never truly understand something without
-            applying it to a problem yourself.
-            <br />
-            <br />
-            i am openly, proudly, and unapologetically a queer transgender
-            woman.
-            <br />
-            <br />
-            technology should be for everyone. i think it's important to create
-            tools and resources that help people express themselves creatively.
-            i want to leave an impact on the world that i can be proud of.
-            <br />
-            <br />
-            be excellent to each other.
-          </p>
+
+          <AboutText />
         </div>
 
         <HCard />
