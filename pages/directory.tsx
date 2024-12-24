0f8645655bea139908eb6c585410ac8329ec7cc9
Brooke Chalmers <breq@breq.dev>
2024-12-23 22:39:12 -0500

    h-card and XFN in directory page

--- pages/directory.tsx
+++ pages/directory.tsx
@@ -55,6 +55,23 @@ export default function Directory() {
         <p className="max-w-2xl text-center font-display text-xl">
           these are my awesome friends. check them out!
         </p>
+        <p className="max-w-2xl text-center font-display text-xl italic">
+          new: this page is{" "}
+          <a
+            className="text-panblue-light"
+            href="https://microformats.org/wiki/h-card"
+          >
+            h-card
+          </a>{" "}
+          and{" "}
+          <a
+            className="text-panblue-light"
+            href="http://www.gmpg.org/xfn/intro"
+          >
+            XFN
+          </a>{" "}
+          compliant!
+        </p>
       </div>
 
       <div className="grid grid-cols-[repeat(auto-fit,min(90vw,24rem))] justify-center gap-8 px-4 py-8">
@@ -72,7 +89,7 @@ export default function Directory() {
             font,
             rel,
           }) => (
-            <div className="group relative" key={name}>
+            <div className="h-card group relative" key={name}>
               <div
                 className="pointer-events-none absolute inset-0 z-0 rounded-2xl transition-transform group-focus-within:translate-x-4 group-focus-within:translate-y-2 group-hover:translate-x-4 group-hover:translate-y-2"
                 style={{
@@ -81,14 +98,14 @@ export default function Directory() {
               />
               <a
                 href={url}
-                className="relative z-10 flex flex-col gap-2 rounded-2xl bg-black p-2 font-display text-white focus-visible:outline"
+                className="u-url relative z-10 flex flex-col gap-2 rounded-2xl bg-black p-2 font-display text-white focus-visible:outline"
                 style={{
                   outlineColor: borderIsTextColor ? textColor : color,
                 }}
                 rel={rel}
               >
                 <h2
-                  className="text-center font-mono text-3xl font-bold lowercase"
+                  className="p-nickname text-center font-mono text-3xl font-bold lowercase"
                   style={{ color: textColor }}
                 >
                   {tag || name.split(" ")[0]}
@@ -101,7 +118,7 @@ export default function Directory() {
                   className="w-full"
                 >
                   {image /* eslint-disable-next-line @next/next/no-img-element */ && (
-                    <img className="w-full" src={image} alt={name} />
+                    <img className="u-logo w-full" src={image} alt={name} />
                   )}
                   {placeholder && (
                     <div className="flex h-full items-center justify-center border-2 border-dashed border-white">
@@ -115,8 +132,8 @@ export default function Directory() {
                   className="text-center"
                   style={{ fontFamily: font ? `"${font}"` : "inherit" }}
                 >
-                  <h2 className="text-4xl font-bold">{name}</h2>
-                  <p>{bio}</p>
+                  <h2 className="p-name text-4xl font-bold">{name}</h2>
+                  <p className="p-note">{bio}</p>
                 </div>
               </a>
             </div>
